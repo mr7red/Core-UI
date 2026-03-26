bcb812bba8276bef2445dba99b09834c61c84ac3
mr7red <ananda27826@gmail.com>
2026-03-26 14:15:37 +0530

    updated backend deploy

--- backend/server.js
+++ backend/server.js
@@ -49,4 +49,8 @@ app.use("/category",require("./routes/category"));
 app.use("/product",require("./routes/product"));
 
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
